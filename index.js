86e68ec5314bcab5b047ff5e4f3cd14811d3ccce
TANABE Ken-ichi <nabeken@tknetworks.org>
2015-12-06 16:38:56 +0900

    index: Add headers and body properly to work with API Gateway

--- index.js
+++ index.js
@@ -23,32 +23,34 @@ function new_request(event) {
   return function request(callback, results) {
     var opts = {
       socketPath: '/tmp/lambda-server.sock',
-      method: 'POST'
+      method: 'POST',
+      headers: event.headers
     }
 
     var req = http.request(opts, function(res) {
       var body = '';
-      console.log('Status:', res.statusCode);
-      console.log('Headers:', JSON.stringify(res.headers));
+      console.log('Response Status:', res.statusCode);
+      console.log('Response Headers:', JSON.stringify(res.headers));
       res.setEncoding('utf8');
       res.on('data', function(chunk) {
         body += chunk;
       });
       res.on('end', function() {
           console.log('Successfully processed HTTP response');
-          console.log('Body:', body);
+          console.log('Response Body:', body);
           callback(null, body);
       });
     });
     req.on('error', function(err) {
       callback(err, null);
     });
-    req.write(JSON.stringify(event));
+    req.write(JSON.stringify(event.body));
     req.end();
   }
 }
 
 exports.handler = function(event, context) {
+  console.log("DEBUG: " + JSON.stringify(event.body));
   async.retry({times: 3, interval: 200}, new_request(event), function(err, result) {
     if (err) {
       context.fail(err);
